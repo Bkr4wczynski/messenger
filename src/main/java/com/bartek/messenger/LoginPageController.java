4144cc89fd14c12a781bf12a7bf83f4aea047385
Bkr4wczynski <bkrawczynski@protonmail.com>
2024-05-12 13:39:32 +0200

    Created simple login and sign up pages

--- src/main/java/com/bartek/messenger/LoginPageController.java
+++ src/main/java/com/bartek/messenger/LoginPageController.java
@@ -1,7 +1,36 @@
 package com.bartek.messenger;
 
-public class LoginPageController {
-    public void login(){
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+
+public class LoginPageController {
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private Label warningsLabel;
+
+    public void redirectToSignUpPage(ActionEvent event) throws IOException {
+        Parent parent = FXMLLoader.load(MessengerApp.class.getResource("FXML-files/SignUpPage.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(parent);
+        stage.setScene(scene);
+        stage.show();
     }
+
+    public void login(){
+        // tries to login via database
+    }
+
 }
