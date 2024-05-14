56f57a54d874e8a1bf727c891617d3a9f8b029a3
Bkr4wczynski <bkrawczynski@protonmail.com>
2024-05-14 16:21:51 +0200

    added FriendsListPageController.java and FriendsPageController.java

---
+++ src/main/java/com/bartek/messenger/mainPage/FriendsListPageController.java
@@ -0,0 +1,25 @@
+package com.bartek.messenger.mainPage;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class FriendsListPageController {
+    public Label displayLabel;
+    int numberOfOnline = 0;
+    int numberOfFriends = 0;
+    int numberOfWaiting = 0;
+    int numberOfBlocked = 0;
+
+    public void changeDisplayLabelText(ActionEvent event){
+        String buttonText = ((Button) event.getSource()).getText();
+        String newText = switch (buttonText) {
+            case "Available" -> "Available online - " + numberOfOnline;
+            case "All" -> "All friends - " + numberOfFriends;
+            case "Waiting" -> "Waiting - " + numberOfWaiting;
+            case "Blocked" -> "Blocked - " + numberOfBlocked;
+            default -> "";
+        };
+        displayLabel.setText(newText);
+    }
+}
--- src/main/java/com/bartek/messenger/mainPage/FriendsPageController.java
+++ src/main/java/com/bartek/messenger/mainPage/FriendsPageController.java
@@ -1,4 +1,20 @@
 package com.bartek.messenger.mainPage;
 
+import javafx.event.ActionEvent;
+
 public class FriendsPageController {
+    public FriendsListPageController friendsListPageController;
+
+    public void showAvailable(ActionEvent event){
+        friendsListPageController.changeDisplayLabelText(event);
+    }
+    public void showAll(ActionEvent event){
+        friendsListPageController.changeDisplayLabelText(event);
+    }
+    public void showWaiting(ActionEvent event){
+        friendsListPageController.changeDisplayLabelText(event);
+    }
+    public void showBlocked(ActionEvent event){
+        friendsListPageController.changeDisplayLabelText(event);
+    }
 }
