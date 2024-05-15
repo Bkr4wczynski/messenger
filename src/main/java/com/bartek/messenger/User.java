435c3ab41317ee0af75a6bd783d008f9386e83b9
Bkr4wczynski <bkrawczynski@protonmail.com>
2024-05-15 15:25:27 +0200

    created simple user pojo

---
+++ src/main/java/com/bartek/messenger/User.java
@@ -0,0 +1,48 @@
+package com.bartek.messenger;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class User {
+    public String username;
+    private final Set<User> friends;
+    private final Set<User> blocked;
+    private boolean online;
+
+    public User(String username) {
+        this.username = username;
+        friends = new HashSet<>();
+        blocked = new HashSet<>();
+    }
+    public void addFriend(User user){
+        friends.add(user);
+    }
+    public void removeFriend(User user){
+        friends.remove(user);
+    }
+    public void block(User user){
+        friends.remove(user);
+        blocked.add(user);
+    }
+    public void unBlock(User user){
+        blocked.remove(user);
+    }
+    public void goOnline(){
+        online = true;
+    }
+    public void goOffline(){
+        online = false;
+    }
+
+    public boolean isOnline() {
+        return online;
+    }
+
+    public Set<User> getFriends() {
+        return friends;
+    }
+
+    public Set<User> getBlocked() {
+        return blocked;
+    }
+}
