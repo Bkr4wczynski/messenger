01c7ee352b35d1645bed230100d238bcfae803ac
Bkr4wczynski <bkrawczynski@protonmail.com>
2024-05-18 17:36:46 +0200

    you can now navigate between pages in MainPageController

--- src/main/java/com/bartek/messenger/main/MainPageController.java
+++ src/main/java/com/bartek/messenger/main/MainPageController.java
@@ -1,10 +1,31 @@
 package com.bartek.messenger.main;
 
-public class MainPageController {
-    public void changePageToFriendsPage(){
+import com.bartek.messenger.MessengerApp;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
 
+import java.io.IOException;
+import java.util.Objects;
+
+public class MainPageController {
+    public AnchorPane anchorPane;
+
+    public void changePageToFriendsPage(){
+        try {
+            anchorPane.getChildren().clear();
+            anchorPane.getChildren().add(
+                    FXMLLoader.load(Objects.requireNonNull(MessengerApp.class.getResource("FXML-files/main/friendsPage.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
     public void changePageToConversationPage(){
-
+        try {
+            anchorPane.getChildren().clear();
+            anchorPane.getChildren().add(
+                    FXMLLoader.load(Objects.requireNonNull(MessengerApp.class.getResource("FXML-files/main/conversationPage.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
