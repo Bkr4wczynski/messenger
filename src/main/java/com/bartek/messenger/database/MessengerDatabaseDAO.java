f216402f2d8cf32279db3a5c3e76b12eedcc9cd1
Bkr4wczynski <bkrawczynski@protonmail.com>
2024-05-18 19:33:00 +0200

    added simple database connectivity

--- src/main/java/com/bartek/messenger/database/MessengerDatabaseDAO.java
+++ src/main/java/com/bartek/messenger/database/MessengerDatabaseDAO.java
@@ -1,4 +1,36 @@
 package com.bartek.messenger.database;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class MessengerDatabaseDAO {
+    Connection connection;
+    public void openConnection(){
+        try {
+            connection = DataSource.getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void closeConnection(){
+        try {
+            DataSource.closeConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void getUsers(){
+        String query = "SELECT * FROM users";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()){
+                System.out.println(resultSet.getString(2));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/main/java/com/bartek/messenger/server/Server.java
+++ src/main/java/com/bartek/messenger/server/Server.java
@@ -1,7 +1,8 @@
 package com.bartek.messenger.server;
 
+import com.bartek.messenger.database.MessengerDatabaseDAO;
+
 public class Server {
     public static void main(String[] args) {
-
     }
 }
