a703b581aca7dac764a3dbecfa8404abb8747664
Bkr4wczynski <bkrawczynski@protonmail.com>
2024-05-19 12:59:05 +0200

    you can now get user by his username no errors

--- src/main/java/com/bartek/messenger/main/ConversationPageController.java
+++ src/main/java/com/bartek/messenger/main/ConversationPageController.java
@@ -19,7 +19,7 @@ public class ConversationPageController implements Initializable {
     User friend;
     private User getCurrentFriend(){
         // logic
-        return new User("Cwaniak", 5, Gender.Male, LocalDateTime.now());
+        return new User(5, "Cwaniak", Gender.Male, LocalDateTime.now());
     }
     private User getCurrentUser(){
         return UserGetter.GET_CURRENT_USER();
